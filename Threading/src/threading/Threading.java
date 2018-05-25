6b8e818783fae367d672333b9e9f48c461f9568d
Adriannosaurus <Adriannosaurus@adrianncomputer.fios-router.home>
2018-05-24 21:50:26 -0400

    Basic structure AKA threads are running

--- Threading/src/threading/Threading.java
+++ Threading/src/threading/Threading.java
@@ -4,6 +4,28 @@ package threading;
 import java.util.*;
 import java.io.*;
 
+public class Threading implements Runnable
+{    
+    int i = 1;
+    @Override
+    public void run()
+    {
+        System.out.println("Hello from thread " + i++ + "!");
+    }
+    
+    
+    public static void main(String[] args)
+    {
+        Threading myThreading = new Threading();
+        Thread t = new Thread(myThreading);
+        t.setPriority(1);
+        t.start();
+        
+        Thread t2 = new Thread(myThreading);
+        t2.setPriority(2);
+        t2.start();
+    }
+    
 /*
 public class Threading extends Thread
 {    
@@ -25,28 +47,6 @@ public class Threading extends Thread
     }
     
 }
-
 */
-
-public class Threading implements Runnable
-{    
-    
-    @Override
-    public void run()
-    {
-        //I think this is thread 2
-    }
-    
-    
-    public static void main(String[] args)
-    {
-        Threading myThreading = new Threading();
-        Thread t = new Thread(myThreading);
-        t.setPriority(1);
-        t.start();
-
-        //This should be thread 1
-
-    }
     
 }
