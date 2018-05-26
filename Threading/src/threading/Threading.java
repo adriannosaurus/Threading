8b8164766b74312f8489a5f7ccc677e1c66a8a0b
Adriannosaurus <Adriannosaurus@adrianncomputer.fios-router.home>
2018-05-26 11:13:12 -0400

    Starting to make objects and methods for deadlock scenario

--- Threading/src/threading/Threading.java
+++ Threading/src/threading/Threading.java
@@ -6,24 +6,46 @@ import java.io.*;
 
 public class Threading implements Runnable
 {    
-    int i = 1;
-    @Override
-    public void run()
-    {
-        System.out.println("Hello from thread " + i++ + "!");
-    }
-    
-    
     public static void main(String[] args)
     {
         Threading myThreading = new Threading();
-        Thread t = new Thread(myThreading);
-        t.setPriority(1);
-        t.start();
+        Thread t1 = new Thread(myThreading);
+        t1.setName("Harry Potter");
+        t1.setPriority(1);
+        t1.start();
         
         Thread t2 = new Thread(myThreading);
+        t2.setName("Ron Weasley");
         t2.setPriority(2);
         t2.start();
+    }
+    
+    @Override
+    public synchronized void run()
+    {
+        Inventory inv = new Inventory((Thread.currentThread().getPriority() * 100));
+        System.out.println(Thread.currentThread().getName() + "'s inventory is " + inv.getStock());
+    }
+    
+    private static class Inventory
+    {
+        private int stock;
+        public Inventory(int i)
+        {
+            this.stock = i;
+        }
+        
+        public int getStock()
+        {
+            return stock;
+        }
+    }
+    
+    int num = 0;
+    public synchronized void addNum(int x)
+    {
+        num += x;
+        System.out.println("   Number: " + num);
     }
     
 /*
