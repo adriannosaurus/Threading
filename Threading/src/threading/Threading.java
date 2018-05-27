4f5fa76e5f54b73fc0df90231eedbc2b7401bcb5
Laura Levy <llevy510@gmail.com>
2018-05-27 13:56:07 -0400

    created new thread classes w logic

--- Threading/src/threading/Threading.java
+++ Threading/src/threading/Threading.java
@@ -4,49 +4,147 @@ package threading;
 import java.util.*;
 import java.io.*;
 
-public class Threading implements Runnable
+public class Threading //implements Runnable
 {    
+
+	public static Object Lock1 = new Object();
+	public static Object Lock2 = new Object();
+	
     public static void main(String[] args)
     {
-        Threading myThreading = new Threading();
-        Thread t1 = new Thread(myThreading);
+        //Threading myThreading = new Threading();
+        //Thread t1 = new Thread(myThreading);
+        harrythread t1 = new harrythread();
         t1.setName("Harry Potter");
         t1.setPriority(1);
         t1.start();
         
-        Thread t2 = new Thread(myThreading);
+        ronthread t2 = new ronthread();
         t2.setName("Ron Weasley");
         t2.setPriority(2);
         t2.start();
     }
     
-    @Override
-    public synchronized void run()
-    {
-        Inventory inv = new Inventory((Thread.currentThread().getPriority() * 100));
-        System.out.println(Thread.currentThread().getName() + "'s inventory is " + inv.getStock());
-    }
-    
     private static class Inventory
     {
         private int stock;
+        private int soldStock;
         public Inventory(int i)
         {
             this.stock = i;
+            this.soldStock= 1000-stock;
         }
         
         public int getStock()
         {
             return stock;
         }
+        public int getSoldStock()
+        {
+        	return (1000-stock);
+        }
+        public void makeSale(int sell) 
+        {
+        	
+        	if(stock<sell) {
+            	System.out.println("insufficient stock");
+            }
+        	else{
+        		stock = stock - sell;
+        		soldStock = 1000-stock;
+        		System.out.println("sale complete");
+        	}
+        	
+        }
+        public void orderStock(int order)
+        {
+        	if(soldStock !=stock)
+        	{
+
+        		System.out.println("must sell all stock before ordering more stock");
+        	}
+        	else
+        	{
+        		stock = stock + order;
+            	soldStock=1000-stock;
+        	}
+        }
     }
     
-    int num = 0;
-    public synchronized void addNum(int x)
+    private static class harrythread extends Thread {
+    	public void run() {
+    		synchronized (Lock1) {
+    			int inventory = Thread.currentThread().getPriority() * 100;
+    			Inventory inv = new Inventory(inventory);
+
+    			int stock = inv.getStock();
+    			int soldStock = inv.getSoldStock();
+    			
+    			System.out.println(Thread.currentThread().getName() + "'s inventory is " + stock);
+    			//System.out.println("harry: Holding lock 1...");
+    			
+    			try {
+    				Thread.sleep(10);
+    			}catch(InterruptedException e) {}
+    			System.out.println(Thread.currentThread().getName() + "'s sold inventory is " + soldStock);
+    			//System.out.println("Harry: Holding lock 2");
+    			int order = 1000;
+                System.out.println("Harry: recieving order for "+order+" units");
+                inv.makeSale(order);
+    			
+    			synchronized(Lock2) {
+                    System.out.println("Harry: Holding to order stock...");
+                    inv.orderStock(order);
+    			}
+    		}
+    	}
+    }
+    private static class ronthread extends Thread {
+    	public void run() {
+    		synchronized (Lock2) {
+    			Inventory inv = new Inventory((Thread.currentThread().getPriority() * 100));
+
+    			int stock = inv.getStock();
+    			int soldStock = inv.getSoldStock();
+    			
+    			System.out.println(Thread.currentThread().getName() + "'s inventory is " + stock);
+                //System.out.println("Ron: Holding lock 1...");
+               
+                try {
+                   Thread.sleep(10);
+                } catch (InterruptedException e) {}
+                System.out.println(Thread.currentThread().getName() + "'s sold inventory is " + soldStock);
+                //System.out.println("Ron: Waiting for lock 2...");
+                int order = 1000;
+                System.out.println("Ron: recieving order for "+order+" units");
+                inv.makeSale(order);
+                
+                
+                synchronized (Lock1) {
+                   System.out.println("Ron: Holding to order stock...");
+                   inv.orderStock(order);
+                }
+    		}
+    	}
+    }
+    
+    /*@Override
+    public synchronized void run()
     {
-        num += x;
+        Inventory inv = new Inventory((Thread.currentThread().getPriority() * 100));
+        System.out.println(Thread.currentThread().getName() + "'s inventory is " + inv.getStock());
+    }*/
+    
+    
+    
+    /*int num = 0;
+    /*public synchronized void addNum(int x)
+    {
+        
+    	num += x;
         System.out.println("   Number: " + num);
     }
+    
     
 /*
 public class Threading extends Thread
